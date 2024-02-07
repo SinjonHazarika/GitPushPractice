9c477967a5ae05638fd1a48343944f906276936e
Sinjon Hazarika <Sinjon Hazarika@TYSS-Sinjon-Hazarika>
2024-02-07 17:16:11 +0530

    First commit

---
+++ GitHubPractice/src/main/java/GenericLibrary/sinjon.java
@@ -0,0 +1,20 @@
+package GenericLibrary;
+
+public class sinjon {
+
+	String name = "Sinjon";
+	int age = 24;
+	
+	public void work() {
+		System.out.println("Working...");
+	}
+	
+	public void walk() {
+		System.out.println("Walking...");
+	}
+	
+	public void read() {
+		System.out.println("Reading...");
+	}
+	
+}
